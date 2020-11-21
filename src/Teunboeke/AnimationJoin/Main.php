90926337a4cefd352fe3c71bf28f6b79644851c5
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-11-21 16:38:10 +0100

    Update Main.php

--- src/Teunboeke/AnimationJoin/Main.php
+++ src/Teunboeke/AnimationJoin/Main.php
@@ -1,1 +1,14 @@
 <?php
+
+declare(strict_types=1);
+
+namespace Teunboeke\AnimationJoin;
+
+use pocketmine\event\listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\item\item;
+use pocketmine\network\mcpe\protocol\ActorEventPacket;
+use pocketmine\network\mcpe\protocol\LevelEventPacket;
+
+class Main implements Listener
+  
